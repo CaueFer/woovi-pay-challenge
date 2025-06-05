712a97610bb8b4ff4875e88d7aa403c70671e860
cauefer <cauefernandescaetano@gmail.com>
2025-06-05 09:49:16 -0300

    ` feat `: Adiciona client Graphql e Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import { join } from 'node:path';
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, type ApolloDriverConfig } from '@nestjs/apollo';
+
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+import { typeOrmConfig } from './config/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/createUser.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async getAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async saveUser(newUser: CreateUserDto): Promise<string> {
+    const user = await this.userRepository.save(newUser);
+    return user.id;
+  }
+}
