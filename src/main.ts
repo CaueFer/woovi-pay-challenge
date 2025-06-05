06df4cc05e38edd8556c278c44dafe02d92ae76d
cauefer <cauefernandescaetano@gmail.com>
2025-06-05 10:22:04 -0300

    ` feat `: Adiciona o class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000).then(() => {
     console.log('Server running on port 3000');
   });
 }
-bootstrap();
+void bootstrap();
--- src/user/dto/createUser.dto.ts
+++ src/user/dto/createUser.dto.ts
@@ -1,7 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   @Field()
   name: string;
 }
