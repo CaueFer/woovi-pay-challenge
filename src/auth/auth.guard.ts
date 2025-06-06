7d8e549fdf0ab4e02237ac3f569b97949a02a9d9
cauefer <cauefernandescaetano@gmail.com>
2025-06-06 13:57:29 -0300

    ` fix `: Arruma validao de tipo do Request (rest, graphql, ws)

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,15 +1,17 @@
 import {
   CanActivate,
   ExecutionContext,
+  HttpException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { IS_PUBLIC_KEY } from 'src/lib/decorators/skipAuth.decorator';
 
+import { IS_PUBLIC_KEY } from 'src/lib/decorators/skipAuth.decorator';
 import { User } from 'src/lib/entity/user.entity';
 
 @Injectable()
@@ -21,24 +23,41 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (isPublic) return true;
-
-    const request = context.switchToHttp().getRequest<Request>();
-    const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
-    }
     try {
+      const isPublic = this.reflector.getAllAndOverride<boolean>(
+        IS_PUBLIC_KEY,
+        [context.getHandler(), context.getClass()],
+      );
+      if (isPublic) return true;
+
+      let request: Request;
+
+      // HTTP - REST
+      request = context.switchToHttp().getRequest<Request>();
+
+      // SE NAO FOR REST
+      if (!request) {
+        const gqlCtx: { req: unknown } =
+          GqlExecutionContext.create(context).getContext();
+
+        if (!gqlCtx || !gqlCtx.req)
+          throw new HttpException('Erro na requisição', 500);
+
+        request = gqlCtx.req as Request;
+      }
+
+      const token = this.extractTokenFromHeader(request);
+      if (!token) {
+        throw new UnauthorizedException();
+      }
+
       const payload: Partial<User> = await this.jwtService.verifyAsync(token, {
         secret: this.config.get<string>('JWT_SECRET'),
       });
       request['user'] = payload;
-    } catch {
-      throw new UnauthorizedException();
+    } catch (err) {
+      console.log('Erro auth guard: ', err);
+      throw new HttpException('Erro no servidor.', 500);
     }
     return true;
   }
